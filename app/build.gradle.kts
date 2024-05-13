15a81c0f4a0412da1309a986ae2eefab17de1f50
Heshan Kodithuwakku <it22589804@my.sliit.lk>
2024-05-13 08:52:50 +0530

    module app updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,8 +2,9 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-android")
-    id("kotlin-android-extensions")
     id("kotlin-kapt")
+    // Add kotlin-parcelize plugin
+    id("kotlin-parcelize")
 }
 
 android {
@@ -18,6 +19,11 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    // Enable View Binding
+    buildFeatures {
+        viewBinding = true
     }
 
     buildTypes {
@@ -50,7 +56,7 @@ dependencies {
     // room
     def room_version = "2.6.1"
     implementation("androidx.room:room-runtime:$room_version")
-    annotationProcessor "androidx.room:room-compiler:$room_version"
+    annotationProcessor("androidx.room:room-compiler:$room_version")
     kapt("androidx.room:room-compiler:$room_version")
     implementation("androidx.room:room-ktx:$room_version")
 }
